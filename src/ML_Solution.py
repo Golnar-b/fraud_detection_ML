5cae70230781fd7316d7466dd84a4ce195aee1ce
Golnar-b <g.behzadi@auckland.ac.nz>
2020-02-19 09:50:00 +1300

    Add functions to plot the elbow for getting insight on number of clusters for the clsutering model of Kmeans and then train and save the model accordingly

--- src/ML_Solution.py
+++ src/ML_Solution.py
@@ -19,4 +19,28 @@ def load_clean_data(input_data_name):
                         'avg_redemptions']]
     return(orig_data, model_data)
 
-      
+
+def scale_data_select_features(model_data):
+    X = np.array(model_data).astype(np.float)
+    X_scaled = StandardScaler().fit_transform(X)
+    
+    #get the most important features with pca
+    pca = PCA(n_components=7)
+    pca_model = pca.fit(X_scaled)
+    x_7d = pca.transform(X_scaled)
+    
+    #get the index of the most important feature on each component 
+    n_pcs= pca_model.components_.shape[0]
+    most_important = [np.abs(pca_model.components_[i]).argmax() for i in range(n_pcs)]
+    initial_feature_names = model_data.columns.tolist()
+    most_important_names = [initial_feature_names[most_important[i]] for i in range(n_pcs)]
+    dic = {'PC{}'.format(i+1): most_important_names[i] for i in range(n_pcs)}
+    PCA_df = pd.DataFrame(sorted(dic.items()))
+    
+    X_scaled_reduced = x_7d
+    return(X_scaled_reduced, PCA_df)
+    
+    
+def train_and_save_model(X_scaled_reduced, n_clusters, model_filename):
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42).fit(X_scaled_reduced)
+    pickle.dump(kmeans, open(model_filename, 'wb'))
